675bbafcbb88fda61398c573229dd90c27b41528
Christopher Wilson <cwilson@broadinstitute.org>
2020-05-02 09:22:28 -0400

    Update STAR1stPASS.sh

--- STAR1stPASS.sh
+++ STAR1stPASS.sh
@@ -8,18 +8,10 @@
 #SBATCH --mem=80000
 #SBATCH -t 30:00:00
 
-one=/n/holyscratch01/liu_lab/cwilson/cwilson/cwilson/Genomes/STAR/Mouse/STAR/mm10_LMNA_Cas9/STAR
-two=/n/holyscratch01/liu_lab/cwilson/Progeria_Mouse/P3/heart/reads
-three=/n/holyscratch01/liu_lab/cwilson/Progeria_Mouse/P3/heart/reads
+one=Path_to_indexed_genome
+two=Directory_containing_trimmed_reads
 
 module purge
 module load STAR/2.7.0e-fasrc01
 
-# $one == path to directory where STAR index lives
-# $two == R1 fastq file
-# $three == R2 fastq file
-# The --readFilesCommand is set to zcat under the assumption that the fastq files are provided
-# as gzipped files. The number of threads, total memory, and length of job will depend upon
-# a number of factors including the library size, size of genome, etc.
-
-STAR --runThreadN 16 --genomeDir $one --outFileNamePrefix 1_1pass --readFilesIn $two/1_R1.fastq $three/1_R2.fastq
+STAR --runThreadN 16 --genomeDir $one --outFileNamePrefix 1_1pass --readFilesIn $two/1_trimmed_R1.fastq $two/1_trimmed_R2.fastq
