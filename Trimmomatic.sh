7b9bc3efd7993dead0ef928b90290544c817acac
Christopher Wilson <cwilson@broadinstitute.org>
2020-05-02 09:17:18 -0400

    Update Trimmomatic.sh

--- Trimmomatic.sh
+++ Trimmomatic.sh
@@ -8,8 +8,8 @@
 #SBATCH -J trimmomatic_chupa
 #SBATCH -t 1:00:00
 
-DIR=1
-OUT_DIR=2 
+one=Directory_containing_reads
+two=Out_Directory
 module load jdk
 
-java -jar ~/apps/Trimmomatic-0.35/trimmomatic-0.35.jar PE $1/1_R1.fastq.gz $1/29_R2.fastq.gz $2/1_trimmed_R1.fq $2/1_unpaired_R1.fq $2/1_trimmed_R2.fq $2/1_unpaired_R2.fq SLIDINGWINDOW:4:30 TRAILING:30 ILLUMINACLIP:TruSeq2-PE.fa:2:30:5
+java -jar ~/apps/Trimmomatic-0.35/trimmomatic-0.35.jar PE $one/1_R1.fastq.gz $one/29_R2.fastq.gz $two/1_trimmed_R1.fq $two/1_unpaired_R1.fq $two/1_trimmed_R2.fq $two/1_unpaired_R2.fq SLIDINGWINDOW:4:30 TRAILING:30 ILLUMINACLIP:TruSeq2-PE.fa:2:30:5
